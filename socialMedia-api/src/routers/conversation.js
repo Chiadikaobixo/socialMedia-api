c609b8e46149fd0fd70068a7d4f29b92892e64b8
chiadikaobixo <chiadikaobixo@gmail.com>
2022-05-27 05:57:41 -0500

    modified conversation

--- socialMedia-api/src/routers/conversation.js
+++ socialMedia-api/src/routers/conversation.js
@@ -4,6 +4,6 @@ const conversationControllers = require('../controllers/conversationControllers'
 
 router.post('/conversations', conversationControllers.conversation)
 
-router.get('/:userId', conversationControllers.getConveration)
+router.get('/conversations/:userId', conversationControllers.getConveration)
 
 module.exports = router

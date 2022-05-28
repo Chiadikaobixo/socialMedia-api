c07807307b798d74cce97d44021e20648f312be5
chiadikaobixo <chiadikaobixo@gmail.com>
2022-05-28 09:26:31 -0500

    modified messages

--- socialMedia-api/src/routers/messages.js
+++ socialMedia-api/src/routers/messages.js
@@ -4,6 +4,6 @@ const messageControllers = require('../controllers/messageControllers')
 
 router.post('/messages', messageControllers.message)
 
-router.get('/:conversationId', messageControllers.getmessage)
+router.get('/messages/:conversationId', messageControllers.getmessage)
 
 module.exports = router

e448e907b324267445055a1774380b72fe9dc8e1
chiadikaobixo <chiadikaobixo@gmail.com>
2022-06-09 18:23:31 -0500

    minor changes

--- socialMedia-api/src/index.js
+++ socialMedia-api/src/index.js
@@ -11,7 +11,7 @@ const conversationRouter = require('./routers/conversation')
 
 app.use(cors())
 app.use((_req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://chiadimedia.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()

d086e77cfe1b51fc48a4e8cb2bac8096cd8b4c08
chiadikaobixo <chiadikaobixo@gmail.com>
2022-06-13 08:30:04 -0500

    modified timeline post

--- socialMedia-api/src/services/postServices.js
+++ socialMedia-api/src/services/postServices.js
@@ -61,6 +61,8 @@ class PostServices {
     async timelinePost(userId) {
         const currentUser = await User.findById({ _id: userId })
         const userPost = await Post.find({ userId: currentUser._id })
+        if(!userPost) throw new CustomError('userId does not exist')
+
         const friendsPost = await Promise.all(
             currentUser.followings.map((friendId) => {
                 return Post.find({ userId: friendId })

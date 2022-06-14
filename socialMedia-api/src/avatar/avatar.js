2d45b44e164435b84b463efba71940b5fc784c3e
chiadikaobixo <chiadikaobixo@gmail.com>
2022-06-14 18:47:33 -0500

    modified avatar

--- socialMedia-api/src/avatar/avatar.js
+++ socialMedia-api/src/avatar/avatar.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
         cb(null, "public/assets" );
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
+        cb(null, req.body.name);
     },
 });
 

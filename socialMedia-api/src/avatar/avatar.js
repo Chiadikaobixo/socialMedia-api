7de700401861f34cc0ebe9e1b8a6c99e8f1a2bd3
chiadikaobixo <chiadikaobixo@gmail.com>
2022-06-15 05:37:30 -0500

    modified avatar

--- socialMedia-api/src/avatar/avatar.js
+++ socialMedia-api/src/avatar/avatar.js
@@ -4,7 +4,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "https://chiadi-socialmedia-api.herokuapp.com/public/assets" );
+        cb(null, "public/assets" );
     },
     filename: (req, file, cb) => {
         cb(null, req.body.name);
